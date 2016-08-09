6e0c04a703e0408adffdcf9748618ea5e22d254a
Nomi Vos <nvoswork@gmail.com>
2016-08-09 06:14:25 -0500

    closes #11, adds description to listing

--- spec/features/new_space_spec.rb
+++ spec/features/new_space_spec.rb
@@ -4,9 +4,12 @@ feature 'Create New Space' do
     visit '/spaces/new'
     fill_in 'Name of Space:', with: 'The White House'
     fill_in 'Address:', with: '1600 Pennsylvania Avenue'
+    fill_in 'Description', with: 'A BIG WHITE house'
     expect{ click_button 'Add Space' }.to change(Space, :count).by(1)
     space = Space.first
     expect(space.name).to eq 'The White House'
+    expect(page).to have_content 'The White House'
+    expect(page).to have_content 'A BIG WHITE house'
 
   end
 
