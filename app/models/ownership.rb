3fc5a662806f94154d45cd90776fb3e81ff08f98
liskowsky <karollebiedzinski@gmail.com>
2016-08-11 10:46:26 +0200

    Thursday standup notes

---
+++ app/models/ownership.rb
@@ -0,0 +1,9 @@
+class Ownership
+  include DataMapper::Resource
+
+  property :id, Serial
+
+  belongs_to :user
+  belongs_to :space
+
+end
--- app/models/space.rb
+++ app/models/space.rb
@@ -3,11 +3,16 @@ class Space
   include DataMapper::Resource
 
   has 1, :user, through: Resource
+  #has n, :bookings
+  #has n, :users, :through => :bookings
+  #has 1, :ownership
+  #has 1, :user, :through => :ownership
 
   property :id, Serial
   property :name, String, required: true
   property :address, String, required: true
   property :description, Text
   property :price, Decimal, precision: 15, scale: 4
+  #property :price_for_night, Decimal, precision: 15, scale: 4
 
 end
